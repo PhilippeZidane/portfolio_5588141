89c0bb0ac2fbb0ec6958527be2ce5c8068bbeeb1
PhilippeZidane <Fotsophilippe237@gmail.com>
2025-05-10 17:27:04 +0200

    commiting main.dart und Homepage.dart

--- my_portfolio/lib/main.dart
+++ my_portfolio/lib/main.dart
@@ -20,3 +20,27 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+/*
+import 'package:flutter/material.dart';
+import 'pages/Homepage.dart';
+
+// ======== ORIGINAL MAIN + MyApp ========
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Portfolio',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(), // Calls the home page
+    );
+  }
+}
+*/
--- my_portfolio/lib/pages/Homepage.dart
+++ my_portfolio/lib/pages/Homepage.dart
@@ -1,4 +1,21 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Willkommen im Portfolio von Fotso Philippe'),
+      ),
+      body: const Center(
+        child: Text('Hier kommt dein Inhalt hin.'),
+      ),
+    );
+  }
+}
+/*
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,4 +32,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
+}*/
